3e5def775682eb2631c03e4d5dfc3dd449ddac27
Lance Marks <lance.marks@pendo.io>
2021-12-04 03:09:26 -0500

    after day 4

--- 04/problem.py
+++ 04/problem.py
@@ -1,14 +1,113 @@
+import time
+from os import wait
+
+from numpy import remainder
 from utils.utility import *
 import pathlib
 DATA_FILE_PATH = str(pathlib.Path().resolve()) + '/04/data.txt'
 
 
+def mark_bingo_numbers(number, boards, masks):
+    temp_mask = np.isin(boards, number)
+    masks[temp_mask] = 1
+    print("after marking top 2")
+    print(boards[:2])
+    print(masks[:2])
+    return masks
+
+
+def sum_unmarked_numbers(board, mask):
+    temp_mask = np.isin(mask, 0)
+    return np.sum(board[temp_mask])
+
+
+def check_for_win(board):
+    # check across
+    print('checking board')
+    print(board)
+    print(np.sum(board, axis=0))
+    if 5 in np.array(np.sum(board, axis=0)).tolist():
+        return True
+    print(np.sum(board, axis=1))
+    if 5 in np.array(np.sum(board, axis=1)).tolist():
+        return True
+    print(np.trace(board))
+    if np.trace(board, dtype="i") == 5:
+        return True
+    print(np.trace(board, 4))
+    if np.trace(board, offset=4, dtype="i") == 5:
+        return True
+
+
 def problem1():
     print('problem 1')
+    bingo_numbers, bingo_boards, mask_boards = load_bingo_into_array(
+        DATA_FILE_PATH)
+    escape = False
+    winning_number = -1
+    winning_mask = None
+    winning_board = None
+    for number in bingo_numbers:
+        mask_boards = mark_bingo_numbers(number, bingo_boards, mask_boards)
+        for index, mask in enumerate(mask_boards):
+            if(check_for_win(mask)):
+                winning_board = bingo_boards[index]
+                winning_mask = mask
+                winning_number = number
+                escape = True
+                break
+        if escape:
+            break
+
+    # calculate answer stuff
+    unmarked_sum = sum_unmarked_numbers(winning_board, winning_mask)
+    total = winning_number * unmarked_sum
+    print("total is {}".format(total))
+    print("GAME OVER")
+
+    # 35711
 
 
 def problem2():
     print('problem 2')
+    bingo_numbers, bingo_boards, mask_boards = load_bingo_into_array(
+        DATA_FILE_PATH)
+    escape = False
+    winning_number = -1
+    winning_mask = None
+    winning_board = None
+
+    for number in bingo_numbers:
+        mask_boards = mark_bingo_numbers(number, bingo_boards, mask_boards)
+
+        for index in range(mask_boards.shape[0]-1, -1, -1):
+            if(check_for_win(mask_boards[index])):
+                if (mask_boards.shape[0] > 1):
+                    winning_number = number
+                    winning_mask = mask_boards[index]
+                    winning_board = bingo_boards[index]
+
+                    mask_boards = np.delete(mask_boards, index, axis=0)
+                    bingo_boards = np.delete(bingo_boards, index, axis=0)
+                else:
+                    winning_board = bingo_boards[index]
+                    winning_mask = mask_boards[index]
+                    winning_number = number
+                    escape = True
+                    break
+        if escape:
+            break
+    # calculate answer stuff
+    # print("winning board")
+    # print(winning_board)
+    # print(winning_mask)
+    # print(winning_number)
+    unmarked_sum = sum_unmarked_numbers(winning_board, winning_mask)
+    total = winning_number * unmarked_sum
+    print("total is {}".format(total))
+    print("GAME OVER")
+
+    # 5586
 
 
 def main():
--- utils/utility.py
+++ utils/utility.py
@@ -76,6 +76,61 @@ def keep_match_values(data, index, test):
             data_array.append(curr)
     return data_array
 
+# BINGO
+
+
+def load_bingo_into_array(filename):
+    file = open(filename, "r")
+    bingo_numbers = []
+    bingo_boards = []
+    mask_boards = []
+    bingo_board = []
+    temp_array = []
+    line_count = 0
+    for line in file:
+        line = line.strip()
+        if len(line) < 3:
+            line_count = 0
+            temp_array = []
+            continue
+        elif len(line) > 15:
+            line_count = 0
+            line = line.split(',')
+            line = list(map(int, line))
+            bingo_numbers = np.array(line, dtype="i")
+            # print('Bingo Numbers')
+            # print(bingo_numbers)
+        else:
+            temp_array.append(line.split())
+            # print(len(temp_array))
+            # print(temp_array)
+            line_count += 1
+            if line_count == 5:
+                # print('temp_array')
+                # print(temp_array)
+                board = np.array(temp_array, dtype="i")
+                print(board)
+                print(board.shape)
+                board = board.reshape(5, 5)
+                # print(board)
+                bingo_boards.append(board)
+                mask_boards.append(np.zeros(board.shape, dtype="i"))
+                temp_array = []
+    # print("bingo boards: \n")
+    # print(bingo_boards)
+    bingo_boards = np.array(bingo_boards)
+    mask_boards = np.array(mask_boards)
+    # print(bingo_boards)
+    # print('Bingo Numbers')
+    # print(bingo_numbers)
+
+    file.close()
+    return bingo_numbers, bingo_boards, mask_boards
+
+
+def create_bingo_board():
+    print('making')
+
 
 def test_function():
     print('HelloWorld')
