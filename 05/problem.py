b7c6fe90dd92e31c5724385e008f86d8566000a7
Lance Marks <lance.marks@pendo.io>
2021-12-06 23:56:27 -0500

    through day 6

--- 05/problem.py
+++ 05/problem.py
@@ -1,19 +1,94 @@
 from utils.utility import *
 import pathlib
+import re
+import math
 DATA_FILE_PATH = str(pathlib.Path().resolve()) + '/05/data.txt'
+
+
+def convert_vectors_to_points(array):
+    data = []
+    for vector in array:
+        points = re.findall('\d+', vector)
+        points = [int(point) for point in points]
+        data.append(points)
+    data_out = np.zeros((len(data), 4), dtype="i")
+    data_out[:] = data
+    return data_out
+
+
+def find_max_coords(array):
+    max_vals = array.max(axis=0)
+    return max_vals[0] if max_vals[0] > max_vals[2] else max_vals[2], max_vals[1] if max_vals[1] > max_vals[3] else max_vals[3]
+
+
+def length_of_diagonal(x1, y1, x2, y2):
+    return int(math.sqrt((pow((x2 - x1), 2)**2)+(pow((y2 - y1), 2)**2)))
+
+
+def draw_vector(topo, vector, diagonal):
+    if vector[0] != vector[2] and vector[1] != vector[3] and not diagonal:
+        return topo
+    if vector[0] != vector[2] and vector[1] != vector[3]:
+        x1, y1, x2, y2 = vector
+        dif_y = 1 if y1 < y2 else -1
+        dif_x = 1 if x1 < x2 else -1
+        distance = 0
+        if dif_x > 0:
+            distance = x2 - x1
+        else:
+            distance = x1 - x2
+        for i in range(distance+1):
+            topo[x1, y1] += 1
+            x1 += dif_x
+            y1 += dif_y
+        return topo
+    else:
+        x_range = [vector[0], vector[2]]
+        x_range.sort()
+        y_range = [vector[1], vector[3]]
+        y_range.sort()
+        for i in range(x_range[0], x_range[1]+1):
+            for j in range(y_range[0], y_range[1]+1):
+                topo[i, j] += 1
+        return topo
+
+
+def find_elevation_higher(topo, elevation):
+    print("in find")
+    print("max value is {}".format(topo.max()))
+    return (topo > elevation).sum()
 
 
 def problem1():
     print('problem 1')
+    data = load_file_into_array(DATA_FILE_PATH)
+    data = convert_vectors_to_points(data)
+    maxX, maxY = find_max_coords(data)
+    sea_floor = np.zeros((maxX+1, maxY+1), dtype="i")
+    for vector in data:
+        sea_floor = draw_vector(sea_floor, vector, False)
+    total = find_elevation_higher(sea_floor, 1)
+    print("topo points is {}".format(total))
+    # 6113
 
 
 def problem2():
     print('problem 2')
+    data = load_file_into_array(DATA_FILE_PATH)
+    data = convert_vectors_to_points(data)
+    maxX, maxY = find_max_coords(data)
+    sea_floor = np.zeros((maxX+1, maxY+1), dtype="i")
+    for vector in data:
+        sea_floor = draw_vector(sea_floor, vector, True)
+    total = find_elevation_higher(sea_floor, 1)
+    print("topo points is {}".format(total))
+    # 957972 is too high
+    # 20352 is too low
 
 
 def main():
-    problem1()
-    # problem2()
+    # problem1()
+    problem2()
 
 
 if __name__ == '__main__':
--- 06/problem.py
+++ 06/problem.py
@@ -3,17 +3,66 @@ import pathlib
 DATA_FILE_PATH = str(pathlib.Path().resolve()) + '/06/data.txt'
 
 
+def life_engine(data):
+    count = len(data)
+    for i in range(count):
+        temp_age = data[i] - 1
+        if temp_age < 0:
+            data.append(8)
+            temp_age = 6
+        data[i] = temp_age
+    return data
+
+
+def mass_breeder(data):
+    fish = np.zeros((2, 10), dtype=np.int64)
+    for i in range(10):
+        fish[0][i] = i
+    for i in range(8, 0, -1):
+        fish[1][i-1] = data[1][i]
+    fish[1][8] = data[1][0]
+    fish[1][6] += data[1][0]
+    return fish
+
+
 def problem1():
     print('problem 1')
+    data = load_file_into_array(DATA_FILE_PATH)
+    data = data[0].split(",")
+    data = [int(val) for val in data]
+    # print(data)
+    for i in range(80):
+        data = life_engine(data)
+        # print(data)
+    # print(data)
+    print("total is {}".format(len(data)))
+    # 350149 (80 days)
 
 
 def problem2():
     print('problem 2')
+    data = load_file_into_array(DATA_FILE_PATH)
+    data = data[0].split(",")
+    data = [int(val) for val in data]
+    print(data)
+    fish = np.zeros((2, 10), dtype=np.int64)
+    for i in range(10):
+        fish[0][i] = i
+    print(fish)
+    for i in range(len(data)):
+        fish[1][data[i]] += 1
+    print(fish)
+    data = fish
+    for i in range(256):
+        data = mass_breeder(data)
+    print(data)
+    output = np.array(data[1][:]).sum(axis=0)
+    print("total fish is {}".format(output))
 
 
 def main():
-    problem1()
-    # problem2()
+    # problem1()
+    problem2()
 
 
 if __name__ == '__main__':
